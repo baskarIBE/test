cee410db92fe11a9640e498c516b2dfdb1121497
baskarIBE <baskarIBE@gmail.com>
2022-11-30 14:06:30 +0530

    all file upload2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include ('server.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crackers</title>
+    <title>Crackers2</title>
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">

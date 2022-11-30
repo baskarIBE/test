c2fbad0790e028b7a65fffa5346f119cebc162fb
baskarIBE <baskarIBE@gmail.com>
2022-11-30 12:26:49 +0530

    all file upload1

--- checkout.php
+++ checkout.php
@@ -16,7 +16,6 @@ include ('order.php');
     <link href="img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
 
     <!-- Font Awesome -->
